fee7825bc8da59d42fcd71b3cc1ff0954aaad87c
infoclassesnet-hash <infoclasses.net@gmail.com>
2026-03-14 13:14:02 -0300

    Create public/index.php as the main entry point

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Current Date and Time
+$date = '2026-03-14 16:10:39';
+
+// Main entry point for the application
+// Add application logic here
+
+echo 'Hello, World! Current date and time is: ' . $date;
+
+?>
